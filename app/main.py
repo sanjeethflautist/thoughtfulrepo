178889d1efd9f5cbec7bd9922ab7b8fde176001a
Sanjeeth NAYAK <sanjeeth.nayak@amadeus.com>
2020-12-22 12:28:31 +0530

    v1

--- app/main.py
+++ app/main.py
@@ -1,17 +1,28 @@
 from fastapi import FastAPI
 import sqlite3
 import random
+import os
+
+THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
+dbfilepath = os.path.join(THIS_FOLDER, 'my_data.db')
+
+
 app = FastAPI()
 
 @app.get("/getathought")
-def read_root():
+def getathought():
     id = random.randrange(1000)
-    conn = sqlite3.connect('my_data.db')
+    conn = sqlite3.connect(dbfilepath)
     c = conn.cursor()
     content = c.execute("SELECT QUOTE,AUTHOR FROM QUOTES WHERE ID=" + str(id) + "").fetchall()[0]
     return {content}
 
-
+'''x
 @app.get("/")
 def read_root():
-    return {"URL not supported"}
+    return "Not supported URL"
+
+
+if __name__ == '__main__':
+    read_root()
+'''
