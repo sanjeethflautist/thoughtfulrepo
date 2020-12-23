7009c7300da4840d624e052b0bcf38a42e698fab
Sanjeeth Nayak <sanjeeth.nayak@gmail.com>
2020-12-23 15:38:38 +0530

    V1.1

--- app/main.py
+++ app/main.py
@@ -2,6 +2,11 @@ from fastapi import FastAPI
 import sqlite3
 import random
 import os
+import json
+
+json_data = json.dumps({})
+
+
 
 THIS_FOLDER = os.path.dirname(os.path.abspath(__file__))
 dbfilepath = os.path.join(THIS_FOLDER, 'my_data.db')
@@ -12,17 +17,19 @@ app = FastAPI()
 @app.get("/getathought")
 def getathought():
     id = random.randrange(1000)
-    conn = sqlite3.connect(dbfilepath)
-    c = conn.cursor()
-    content = c.execute("SELECT QUOTE,AUTHOR FROM QUOTES WHERE ID=" + str(id) + "").fetchall()[0]
-    return {content}
+    connection_obj = sqlite3.connect(dbfilepath)
+    cursor_obj = connection_obj.cursor()
+    content = cursor_obj.execute("SELECT QUOTE,AUTHOR FROM QUOTES WHERE ID=" + str(id) + "").fetchall()[0]
+    json_data = json.dumps({'Quote': content[0], 'By':  content[1]})
+    return json_data
 
-'''x
+
 @app.get("/")
 def read_root():
     return "Not supported URL"
 
-
-if __name__ == '__main__':
-    read_root()
 '''
+if __name__ == '__main__':
+    print(getathought())
+'''
+
